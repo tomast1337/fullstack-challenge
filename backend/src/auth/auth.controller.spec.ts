896c3383d816e71087e95a6d104926b19eda259b
tomast1337 <vycasnicolas@gmail.com>
2025-01-27 20:56:26 -0300

    refactor Auth module to use dynamic configuration and improve JWT handling in tests

--- backend/src/auth/auth.controller.spec.ts
+++ backend/src/auth/auth.controller.spec.ts
@@ -1,6 +1,20 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { UserService } from '@server/user/user.service';
+import { JwtService } from '@nestjs/jwt';
+
+const mockUserService = {
+  findByEmail: jest.fn(),
+  findOne: jest.fn(),
+  create: jest.fn(),
+};
+
+const mockJwtService = {
+  decode: jest.fn(),
+  signAsync: jest.fn(),
+  verify: jest.fn(),
+};
 
 describe('AuthController', () => {
   let controller: AuthController;
@@ -8,7 +22,49 @@ describe('AuthController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthController],
-      providers: [AuthService],
+      providers: [
+        AuthService,
+        {
+          provide: UserService,
+          useValue: mockUserService,
+        },
+        {
+          provide: JwtService,
+          useValue: mockJwtService,
+        },
+        {
+          provide: 'COOKIE_EXPIRES_IN',
+          useValue: '3600',
+        },
+        {
+          provide: 'FRONTEND_URL',
+          useValue: 'http://frontend.test.com',
+        },
+        {
+          provide: 'COOKIE_EXPIRES_IN',
+          useValue: '3600',
+        },
+        {
+          provide: 'JWT_SECRET',
+          useValue: 'test-jwt-secret',
+        },
+        {
+          provide: 'JWT_EXPIRES_IN',
+          useValue: '1d',
+        },
+        {
+          provide: 'JWT_REFRESH_SECRET',
+          useValue: 'test-jwt-refresh-secret',
+        },
+        {
+          provide: 'JWT_REFRESH_EXPIRES_IN',
+          useValue: '7d',
+        },
+        {
+          provide: 'APP_DOMAIN',
+          useValue: '.test.com',
+        },
+      ],
     }).compile();
 
     controller = module.get<AuthController>(AuthController);
--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,9 +1,110 @@
-import { Module } from '@nestjs/common';
+import { DynamicModule, Logger, Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { UserModule } from '@server/user/user.module';
+import { MailingModule } from '@server/mailing/mailing.module';
+import { JwtModule } from '@nestjs/jwt';
+import { MagicLinkEmailStrategy } from './strategies/magicLinkEmail.strategy';
+import { JwtStrategy } from './strategies/JWT.strategy';
 
 @Module({
-  controllers: [AuthController],
-  providers: [AuthService],
+  //controllers: [AuthController],
+  //providers: [AuthService],
 })
-export class AuthModule {}
+export class AuthModule {
+  static forRootAsync(): DynamicModule {
+    return {
+      module: AuthModule,
+      imports: [
+        UserModule,
+        ConfigModule.forRoot(),
+        MailingModule,
+        JwtModule.registerAsync({
+          inject: [ConfigService],
+          imports: [ConfigModule],
+          useFactory: async (config: ConfigService) => {
+            const JWT_SECRET = config.get('JWT_SECRET');
+            const JWT_EXPIRES_IN = config.get('JWT_EXPIRES_IN');
+
+            if (!JWT_SECRET) {
+              Logger.error('JWT_SECRET is not set');
+              throw new Error('JWT_SECRET is not set');
+            }
+
+            if (!JWT_EXPIRES_IN) {
+              Logger.warn('JWT_EXPIRES_IN is not set, using default of 60s');
+            }
+
+            return {
+              secret: JWT_SECRET,
+              signOptions: { expiresIn: JWT_EXPIRES_IN || '60s' },
+            };
+          },
+        }),
+      ],
+      controllers: [AuthController],
+      providers: [
+        AuthService,
+        ConfigService,
+        MagicLinkEmailStrategy,
+        JwtStrategy,
+        {
+          inject: [ConfigService],
+          provide: 'COOKIE_EXPIRES_IN',
+          useFactory: (configService: ConfigService) =>
+            configService.getOrThrow<string>('COOKIE_EXPIRES_IN'),
+        },
+        {
+          inject: [ConfigService],
+          provide: 'SERVER_URL',
+          useFactory: (configService: ConfigService) =>
+            configService.getOrThrow<string>('SERVER_URL'),
+        },
+        {
+          inject: [ConfigService],
+          provide: 'MAGIC_LINK_SECRET',
+          useFactory: (configService: ConfigService) =>
+            configService.getOrThrow<string>('MAGIC_LINK_SECRET'),
+        },
+        {
+          inject: [ConfigService],
+          provide: 'FRONTEND_URL',
+          useFactory: (configService: ConfigService) =>
+            configService.getOrThrow<string>('FRONTEND_URL'),
+        },
+        {
+          inject: [ConfigService],
+          provide: 'JWT_SECRET',
+          useFactory: (configService: ConfigService) =>
+            configService.getOrThrow<string>('JWT_SECRET'),
+        },
+        {
+          inject: [ConfigService],
+          provide: 'JWT_EXPIRES_IN',
+          useFactory: (configService: ConfigService) =>
+            configService.getOrThrow<string>('JWT_EXPIRES_IN'),
+        },
+        {
+          inject: [ConfigService],
+          provide: 'JWT_REFRESH_SECRET',
+          useFactory: (configService: ConfigService) =>
+            configService.getOrThrow<string>('JWT_REFRESH_SECRET'),
+        },
+        {
+          inject: [ConfigService],
+          provide: 'JWT_REFRESH_EXPIRES_IN',
+          useFactory: (configService: ConfigService) =>
+            configService.getOrThrow<string>('JWT_REFRESH_EXPIRES_IN'),
+        },
+        {
+          inject: [ConfigService],
+          provide: 'APP_DOMAIN',
+          useFactory: (configService: ConfigService) =>
+            configService.get<string>('APP_DOMAIN'),
+        },
+      ],
+      exports: [AuthService],
+    };
+  }
+}
