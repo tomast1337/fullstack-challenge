40548e34cea61e904cbb176dbeb45a16c299cece
tomast1337 <vycasnicolas@gmail.com>
2025-01-27 22:54:23 -0300

    add: enhance PagingDto with Swagger API properties for better documentation

--- backend/src/dto/paging.dto.ts
+++ backend/src/dto/paging.dto.ts
@@ -1,23 +1,49 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsInt, IsString, IsBoolean, Min, IsOptional } from 'class-validator';
 
 export class PagingDto {
   @IsInt()
   @Min(1)
+  @ApiProperty({
+    default: 1,
+    example: 1,
+    description: 'The page number',
+  })
   page: number;
 
   @IsInt()
-  @Min(1)
+  @Min(25)
+  @ApiProperty({
+    default: 25,
+    example: 25,
+    description: 'The number of items per page',
+  })
   limit: number;
 
   @IsOptional()
   @IsString()
+  @ApiProperty({
+    default: '',
+    example: '',
+    description: 'The search query',
+  })
   query: string;
 
   @IsOptional()
   @IsString()
+  @ApiProperty({
+    default: 'id',
+    example: 'id',
+    description: 'The field to sort by',
+  })
   sort: string;
 
   @IsOptional()
   @IsBoolean()
+  @ApiProperty({
+    default: false,
+    example: false,
+    description: 'The order of the sort',
+  })
   order: boolean;
 }
