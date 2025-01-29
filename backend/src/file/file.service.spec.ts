eb997a51c59afd19bbf3c6e0728019c4a82ee681
tomast1337 <vycasnicolas@gmail.com>
2025-01-29 11:50:01 -0300

    feat: implement image upload and deletion methods in FileService with corresponding tests

--- backend/src/file/file.service.spec.ts
+++ backend/src/file/file.service.spec.ts
@@ -1,10 +1,9 @@
 import {
-  GetObjectCommand,
   HeadBucketCommand,
   PutObjectCommand,
+  DeleteObjectCommand,
   S3Client,
 } from '@aws-sdk/client-s3';
-import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
 import { Test, TestingModule } from '@nestjs/testing';
 
 import { FileService } from './file.service';
@@ -17,6 +16,7 @@ jest.mock('@aws-sdk/client-s3', () => {
   return {
     S3Client: jest.fn(() => mS3Client),
     GetObjectCommand: jest.fn(),
+    DeleteObjectCommand: jest.fn(),
     PutObjectCommand: jest.fn(),
     HeadBucketCommand: jest.fn(),
     ObjectCannedACL: {
@@ -39,12 +39,8 @@ describe('FileService', () => {
       providers: [
         FileService,
         {
-          provide: 'S3_BUCKET_THUMBS',
-          useValue: 'test-bucket-thumbs',
-        },
-        {
-          provide: 'S3_BUCKET_SONGS',
-          useValue: 'test-bucket-songs',
+          provide: 'S3_PICTURE_BUCKET',
+          useValue: 'test-bucket',
         },
         {
           provide: 'S3_KEY',
@@ -66,7 +62,6 @@ describe('FileService', () => {
     }).compile();
 
     fileService = module.get<FileService>(FileService);
-
     s3Client = new S3Client({});
   });
 
@@ -75,13 +70,11 @@ describe('FileService', () => {
   });
 
   describe('verifyBucket', () => {
-    it('should verify the buckets successfully', async () => {
-      (s3Client.send as jest.Mock).mockResolvedValueOnce({});
+    it('should verify the bucket successfully', async () => {
       (s3Client.send as jest.Mock).mockResolvedValueOnce({});
 
       await fileService['verifyBucket']();
 
-      expect(s3Client.send).toHaveBeenCalledWith(expect.any(HeadBucketCommand));
       expect(s3Client.send).toHaveBeenCalledWith(expect.any(HeadBucketCommand));
     });
 
@@ -93,149 +86,68 @@ describe('FileService', () => {
     });
   });
 
-  it('should upload a song', async () => {
-    const buffer = Buffer.from('test');
-    const publicId = 'test-id';
-    const mockResponse = { ETag: 'mock-etag' };
-    (s3Client.send as jest.Mock).mockResolvedValueOnce(mockResponse);
+  describe('uploadImage', () => {
+    it('should upload an image', async () => {
+      const buffer = Buffer.from('test');
+      const id = 'test-id';
+      const mockResponse = { ETag: 'mock-etag' };
+      (s3Client.send as jest.Mock).mockResolvedValueOnce(mockResponse);
 
-    const result = await fileService.uploadSong(buffer, publicId);
-    expect(result).toBe('songs/test-id.nbs');
-    expect(s3Client.send).toHaveBeenCalledWith(expect.any(PutObjectCommand));
+      const result = await fileService.uploadImage(buffer, id);
+      expect(result).toBe(
+        'https://test-bucket.s3.test-region.backblazeb2.com/images/test-id.png',
+      );
+      expect(s3Client.send).toHaveBeenCalledWith(expect.any(PutObjectCommand));
+    });
+
+    it('should throw an error if image upload fails', async () => {
+      const buffer = Buffer.from('test');
+      const id = 'test-id';
+
+      (s3Client.send as jest.Mock).mockRejectedValueOnce(
+        new Error('Upload failed'),
+      );
+
+      await expect(fileService.uploadImage(buffer, id)).rejects.toThrow(
+        'Upload failed',
+      );
+    });
   });
 
-  it('should throw an error if song upload fails', async () => {
-    const buffer = Buffer.from('test');
-    const publicId = 'test-id';
+  describe('deleteImage', () => {
+    it('should delete an image', async () => {
+      const pictureUrl =
+        'https://test-bucket.s3.test-region.backblazeb2.com/images/test-id.png';
+      const mockResponse = {};
+      (s3Client.send as jest.Mock).mockResolvedValueOnce(mockResponse);
 
-    (s3Client.send as jest.Mock).mockRejectedValueOnce(
-      new Error('Upload failed'),
-    );
+      await fileService.deleteImage(pictureUrl);
+      expect(s3Client.send).toHaveBeenCalledWith(
+        expect.any(DeleteObjectCommand),
+      );
+    });
 
-    await expect(fileService.uploadSong(buffer, publicId)).rejects.toThrow(
-      'Upload failed',
-    );
+    it('should throw an error if image deletion fails', async () => {
+      const pictureUrl =
+        'https://test-bucket.s3.test-region.backblazeb2.com/images/test-id.png';
+
+      (s3Client.send as jest.Mock).mockRejectedValueOnce(
+        new Error('Deletion failed'),
+      );
+
+      await expect(fileService.deleteImage(pictureUrl)).rejects.toThrow(
+        'Deletion failed',
+      );
+    });
   });
 
-  it('should get a signed URL for a song download', async () => {
-    const key = 'test-key';
-    const filename = 'test-file.nbs';
-    const mockUrl = 'https://mock-signed-url';
-    (getSignedUrl as jest.Mock).mockResolvedValueOnce(mockUrl);
-
-    const result = await fileService.getSongDownloadUrl(key, filename);
-    expect(result).toBe(mockUrl);
-
-    expect(getSignedUrl).toHaveBeenCalledWith(
-      s3Client,
-      expect.any(GetObjectCommand),
-      { expiresIn: 120 },
-    );
-  });
-
-  it('should throw an error if signed URL generation fails', async () => {
-    const key = 'test-key';
-    const filename = 'test-file.nbs';
-
-    (getSignedUrl as jest.Mock).mockRejectedValueOnce(
-      new Error('Signed URL generation failed'),
-    );
-
-    await expect(fileService.getSongDownloadUrl(key, filename)).rejects.toThrow(
-      'Signed URL generation failed',
-    );
-  });
-
-  it('should upload a thumbnail', async () => {
-    const buffer = Buffer.from('test');
-    const publicId = 'test-id';
-    const mockResponse = { ETag: 'mock-etag' };
-    (s3Client.send as jest.Mock).mockResolvedValueOnce(mockResponse);
-
-    const result = await fileService.uploadThumbnail(buffer, publicId);
-
-    expect(result).toBe(
-      'https://test-bucket-thumbs.s3.test-region.backblazeb2.com/thumbs/test-id.png',
-    );
-
-    expect(s3Client.send).toHaveBeenCalledWith(expect.any(PutObjectCommand));
-  });
-
-  it('should delete a song', async () => {
-    const nbsFileUrl = 'test-file.nbs';
-    const mockResponse = {};
-    (s3Client.send as jest.Mock).mockResolvedValueOnce(mockResponse);
-
-    await fileService.deleteSong(nbsFileUrl);
-    expect(s3Client.send).toHaveBeenCalledWith(expect.any(GetObjectCommand));
-  });
-
-  it('should throw an error if song deletion fails', async () => {
-    const nbsFileUrl = 'test-file.nbs';
-
-    (s3Client.send as jest.Mock).mockRejectedValueOnce(
-      new Error('Deletion failed'),
-    );
-
-    await expect(fileService.deleteSong(nbsFileUrl)).rejects.toThrow(
-      'Deletion failed',
-    );
-  });
-
-  it('should get a song file', async () => {
-    const nbsFileUrl = 'test-file.nbs';
-
-    const mockResponse = {
-      Body: {
-        transformToByteArray: jest
-          .fn()
-          .mockResolvedValueOnce(new Uint8Array([1, 2, 3])),
-      },
-    };
-
-    (s3Client.send as jest.Mock).mockResolvedValueOnce(mockResponse);
-
-    const result = await fileService.getSongFile(nbsFileUrl);
-    expect(result).toEqual(new Uint8Array([1, 2, 3]).buffer);
-    expect(s3Client.send).toHaveBeenCalledWith(expect.any(GetObjectCommand));
-  });
-
-  it('should throw an error if song file retrieval fails', async () => {
-    const nbsFileUrl = 'test-file.nbs';
-
-    (s3Client.send as jest.Mock).mockRejectedValueOnce(
-      new Error('Retrieval failed'),
-    );
-
-    await expect(fileService.getSongFile(nbsFileUrl)).rejects.toThrow(
-      'Retrieval failed',
-    );
-  });
-
-  it('should throw an error if song file is empty', async () => {
-    const nbsFileUrl = 'test-file.nbs';
-
-    const mockResponse = {
-      Body: {
-        transformToByteArray: jest.fn().mockResolvedValueOnce(null),
-      },
-    };
-
-    (s3Client.send as jest.Mock).mockResolvedValueOnce(mockResponse);
-
-    await expect(fileService.getSongFile(nbsFileUrl)).rejects.toThrow(
-      'Error getting file',
-    );
-  });
-
-  it('should get upload a packed song', async () => {
-    const buffer = Buffer.from('test');
-    const publicId = 'test-id';
-    const mockResponse = { ETag: 'mock-etag' };
-    (s3Client.send as jest.Mock).mockResolvedValueOnce(mockResponse);
-
-    const result = await fileService.uploadPackedSong(buffer, publicId);
-    expect(result).toBe('packed/test-id.zip');
-    expect(s3Client.send).toHaveBeenCalledWith(expect.any(PutObjectCommand));
+  describe('getImageUrl', () => {
+    it('should return the correct image URL', async () => {
+      const fileName = 'images/test-id.png';
+      const result = await fileService.getImageUrl(fileName);
+      expect(result).toBe(
+        'https://test-bucket.s3.test-region.backblazeb2.com/images/test-id.png',
+      );
+    });
   });
 });
