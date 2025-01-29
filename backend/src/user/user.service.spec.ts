6e18621716b611bc5488286a8cef148199404888
tomast1337 <vycasnicolas@gmail.com>
2025-01-29 11:36:59 -0300

    test: mock dependencies in UserController and UserService tests

--- backend/src/user/user.service.spec.ts
+++ backend/src/user/user.service.spec.ts
@@ -1,12 +1,24 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserService } from './user.service';
+import { PrismaService } from '@server/prisma/prisma.service';
+import { FileService } from '@server/file/file.service';
 
 describe('UserService', () => {
   let service: UserService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UserService],
+      providers: [
+        UserService,
+        {
+          provide: PrismaService,
+          useValue: {},
+        },
+        {
+          provide: FileService,
+          useValue: {},
+        },
+      ],
     }).compile();
 
     service = module.get<UserService>(UserService);
