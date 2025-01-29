dab71cfffa0ce24f3cedfbc28cfe6172c267c28c
tomast1337 <vycasnicolas@gmail.com>
2025-01-29 11:44:16 -0300

    test: mock dependencies in OrdersController and OrdersService tests

--- backend/src/orders/orders.service.spec.ts
+++ backend/src/orders/orders.service.spec.ts
@@ -1,12 +1,19 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { OrdersService } from './orders.service';
+import { PrismaService } from '@server/prisma/prisma.service';
 
 describe('OrdersService', () => {
   let service: OrdersService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [OrdersService],
+      providers: [
+        OrdersService,
+        {
+          provide: PrismaService,
+          useValue: {},
+        },
+      ],
     }).compile();
 
     service = module.get<OrdersService>(OrdersService);
