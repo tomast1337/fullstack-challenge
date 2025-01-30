b6fcfa9734f41b82bbcb235e8fd8550a2242a000
tomast1337 <vycasnicolas@gmail.com>
2025-01-30 09:31:43 -0300

    feat: add Toaster component to RootLayout for improved user notifications

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -6,6 +6,7 @@ import {
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
+import { Toaster } from '@frontend/components/ui/toaster';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -32,6 +33,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Toaster />
         <SidebarProvider>
           <AppSidebar />
           <main>
